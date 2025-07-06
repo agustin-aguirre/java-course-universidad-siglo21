08b0d05c69edc9223c85bea1aa6637262e62964c
Agustín <agus.aguirre.pro@gmail.com>
2025-07-06 13:10:23 -0300

    Extracted printer classes and finished library demo

--- Clase 3 - SOLID/src/Main.java
+++ Clase 3 - SOLID/src/Main.java
@@ -1,10 +1,9 @@
-import Exceptions.BookAlreadyLendedException;
-import Exceptions.BookNotFoundException;
-import Exceptions.DuplicatedBookException;
-import Exceptions.LibraryException;
+import Exceptions.*;
 import Factories.BookFactory;
 import Factories.FixedDataSizeBookFactory;
 import Models.Book;
+import Printers.LibraryReportPrinter;
+import Printers.ReportPrinter;
 import Repositories.ArrayBookRepository;
 import Repositories.ArrayListBookRepository;
 import Repositories.BookRepository;
@@ -14,107 +13,108 @@ import Services.LoanManagers.LoanManager;
 import Services.LoanManagers.SimpleLoanManager;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Main {
 
     public static void main(String[] args) {
         var factory = new FixedDataSizeBookFactory();
-        runDemoWithArray(factory);
-        println("");
-        runDemoWithArrayList(factory);
+        var printer = new LibraryReportPrinter();
+        runDemoWithArray(factory, printer);
+        printer.println("");
+        runDemoWithArrayList(factory, printer);
     }
 
-    private static void runDemoWithArray(BookFactory factory) {
+    private static void runDemoWithArray(BookFactory factory, ReportPrinter printer) {
         var bookRepo = new ArrayBookRepository(factory.create(50));
         var demoTitle = "===============Array===============";
-        runDemoWithDependencies(factory, bookRepo, new SimpleBookFinder(bookRepo), new SimpleLoanManager(bookRepo), demoTitle);
+        runDemoWithDependencies(factory, bookRepo, new SimpleBookFinder(bookRepo), new SimpleLoanManager(bookRepo), demoTitle, printer);
     }
 
-    private static void runDemoWithArrayList(BookFactory factory) {
+    private static void runDemoWithArrayList(BookFactory factory, ReportPrinter printer) {
         var bookRepo = new ArrayListBookRepository(factory.create(50));
         var demoTitle = "=============ArrayList=============";
-        runDemoWithDependencies(factory, bookRepo, new SimpleBookFinder(bookRepo), new SimpleLoanManager(bookRepo), demoTitle);
+        runDemoWithDependencies(factory, bookRepo, new SimpleBookFinder(bookRepo), new SimpleLoanManager(bookRepo), demoTitle, printer);
     }
 
-    private static void runDemoWithDependencies(BookFactory factory, BookRepository bookRepo, BookFinder bookFinder, LoanManager loanManager, String demoTitle) {
+    private static void runDemoWithDependencies(BookFactory factory, BookRepository bookRepo, BookFinder bookFinder, LoanManager loanManager, String demoTitle, ReportPrinter printer) {
         var library = new DemoLibrary(bookRepo, bookFinder, loanManager);
-        println(demoTitle);
+        printer.println(demoTitle);
+        runLibraryDemo(library, factory, printer);
+        printer.println("===================================");
+    }
 
-        println("Showing all books from Jorge Luis Borges:");
+    private static void runLibraryDemo(Library library, BookFactory factory, ReportPrinter printer) {
+        printer.println("Showing all books from Jorge Luis Borges:");
         var borgesBooks = library.getBooksFromAuthor("Jorge Luis Borges").toArray(Book[]::new);
-        println(borgesBooks);
+        printer.println(borgesBooks);
 
-        println("Lending the first 3 of them...");
+        printer.println("Lending the first 3 of them...");
         library.lendBook(borgesBooks[0].getIsbn());
         library.lendBook(borgesBooks[1].getIsbn());
         library.lendBook(borgesBooks[2].getIsbn());
         var lendedBooks = library.getBooksByAvailability(false).toArray(Book[]::new);
-        println(lendedBooks);
+        printer.println(lendedBooks);
 
         var book = borgesBooks[0];
-        printlnf("Attempting to lend book with ISBN: %s (the first one):", book.getIsbn());
+        printer.printlnf("Attempting to lend book with ISBN: %s (the first one):", book.getIsbn());
         try {
             library.lendBook(book.getIsbn());
         }
         catch (BookAlreadyLendedException e) {
-            printErr(e);
+            printer.printErr(e);
         }
 
-        println("Rerturning the book now...");
+        printer.println("Rerturning the book now...");
         library.returnBook(book.getIsbn());
-        println(book.toString());
+        printer.println(book.toString());
 
-        println("Deleting all but the first three...");
+        printer.println("Deleting all but the first three...");
         String deletedBookIsbn = borgesBooks[3].getIsbn();
         for (Book b: Arrays.stream(borgesBooks).skip(3).toList()) {
             library.deleteBook(b.getIsbn());
         }
         borgesBooks = library.getBooksFromAuthor("Jorge Luis Borges").toArray(Book[]::new);
-        println(borgesBooks);
+        printer.println(borgesBooks);
 
-        printlnf("Attempting to delete an already deleted isbn: %s", deletedBookIsbn);
+        printer.printlnf("Attempting to delete an already deleted isbn: %s", deletedBookIsbn);
         try {
             library.deleteBook(deletedBookIsbn);
         }
         catch (BookNotFoundException e) {
-            printErr(e);
+            printer.printErr(e);
         }
 
         Book newRandomBook = factory.create();
-        printlnf("Adding new book: %s", newRandomBook.toString());
+        printer.printlnf("Adding new book: %s", newRandomBook.toString());
         library.addBook(newRandomBook);
-        println("Trying to add the same book...");
+        printer.println("Trying to add the same book...");
         try {
             library.addBook(newRandomBook);
         }
         catch (DuplicatedBookException e) {
-            printErr(e);
+            printer.printErr(e);
         }
 
-        println("===================================");
+        printer.println("Trying to create books with invalid field values:");
+        printer.println("\tISBN empty: ");
+        trySetField(newRandomBook, b -> b::setIsbn, "", printer);
+        printer.println("\tAuthor empty: ");
+        trySetField(newRandomBook, b -> b::setAuthor, "", printer);
+        printer.println("\tTitle empty: ");
+        trySetField(newRandomBook, b -> b::setTitle, "", printer);
+        printer.println("\tPublishing Year = -1: ");
+        trySetField(newRandomBook, b -> b::setYearPublished, -1, printer);
     }
 
-    private static void println(String msg) {
-        System.out.println(msg);
-    }
-
-    private static void println(Book[] books) {
-        println(Arrays.toString(books));
-    }
-
-    private static void printErr(LibraryException e) {
-        println(String.format("There was an error: %s", e.getMessage()));
-    }
-
-    private static void printlnf(String template, Object... values) {
-        println(String.format(template, values));
+    private static <T> void trySetField(Book book, Function<Book, Consumer<T>> objectSetter, T value, ReportPrinter printer) {
+        try {
+            var setter = objectSetter.apply(book);
+            setter.accept(value);
+        }
+        catch (InvalidBookFieldException e) {
+            printer.printErr(e);
+        }
     }
 }
-
-
-// Subir a youtube ?
-// Entrega: Armar un video en Youtube explicando el código fuente y ejecutando
-// el mismo. En la explicación es necesario que expliquen los conceptos teóricos
-// empleados. También deben enviar el código.
-// Los que manejan el concepto GIT lo pueden hacer por ahí, sino en archivos está bien.
-// Plazo de entrega máximo  lunes 7/07, 9.30 hs
